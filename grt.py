f165fc3f021ad9ab431046ed693fb5a0ca27b6bd
dineshdd86 <42868286+dineshdd86@users.noreply.github.com>
2019-05-27 21:32:01 +0530

    Update grt.py

--- grt.py
+++ grt.py
@@ -1,10 +1,10 @@
-ak=int(input())
-bk=int(input())
-ck=int(input())
+l=list(map(int,input().split()))
+ak=l[0]
+bk=l[1]
+ck=l[2]
 if ak>=bk and ak>=ck:
     print(ak)
 elif bk>=ak and bk>=ck:
     print(bk)
 else:
     print(ck)
-  
